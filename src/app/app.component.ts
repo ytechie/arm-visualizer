0935d87f3f7b28423f017d9bed7db911ee77e7d9
Jason Young <jason@ytechie.com>
2019-01-23 11:30:32 -0800

    Added vsembed flag for extension mode

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ToasterContainerComponent, ToasterService, ToasterConfig } from 'angular2-toaster/angular2-toaster';
+import { Router } from '@angular/router';
 
 import { FEEDBACK_TOAST, TemplateService } from './shared/index';
 import { MenuBarComponent, SidebarComponent, WorkbenchComponent } from './shared/index';
@@ -22,10 +23,26 @@ declare const __moduleName: string;
 export class AppComponent implements OnInit {
   private isRunningInVsCode = false;
 
-  constructor(private templateService: TemplateService) {
+  constructor(private router: Router, private templateService: TemplateService) {
+    let isVsEmbed = window.location.href.indexOf('vsembed') >= 0;
+    console.log(window.location.href);
+    console.log(isVsEmbed);
+    if (isVsEmbed) {
+      this.isRunningInVsCode = true;
+      this.templateService.loadTemplate(
+        `{
+          "$schema": "https://schema.management.azure.com/schemas/2015-01-01/deploymentTemplate.json#",
+          "contentVersion": "",
+          "parameters": {  },
+          "variables": {  },
+          "functions": [  ],
+          "resources": [  ],
+          "outputs": {  }
+      }`);
+    }
+
     window.addEventListener('message', event => {
       this.templateService.loadTemplate(event.data.template);
-      this.isRunningInVsCode = true;
     });
   }
 }
